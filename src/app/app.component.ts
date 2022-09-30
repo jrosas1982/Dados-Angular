9fc82f7378834c8b45b5d1deb987187c94dbe2f7
Juan Pablo <jrosas1982@gmail.com>
2022-09-30 00:05:40 -0300

    Dados - proyecto simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,37 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Dados-Angular';
+
+nroIzq : number;
+nroDer : number;
+dadoIzq : string;
+dadoDer : string;
+result : boolean;
+
+constructor(){
+  this.nroDer=0;
+  this.nroIzq=0;
+  this.dadoDer = "";
+  this.dadoIzq = "";
+  this.result = false;
+}
+
+TirarDados() : void{
+this.nroDer = Math.round( Math.random() * 5) + 1 ;
+this.nroIzq = Math.round( Math.random() * 5) + 1 ;
+
+this.dadoIzq = "../assets/img/dice"+ this.nroIzq +".png";
+this.dadoDer = "../assets/img/dice"+ this.nroDer +".png";
+this.result = this.validarParidad(this.nroDer, this.nroIzq);
+
+}
+validarParidad(derecha : number , izquierda : number) : boolean
+{
+  if(derecha === izquierda)
+      return true;
+  return false;
+}
+
 }
